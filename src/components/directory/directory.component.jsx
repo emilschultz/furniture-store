3648fc51356d04efb837b4922985f6ad05450344
Emil Schultz <emilrschultz@gmail.com>
2021-01-20 16:06:31 +0100

    update menu-items to navigate withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,33 +14,33 @@ class Directory extends React.Component {
                     title: 'Chairs',
                     imageUrl: 'https://www.fredericia.com/Files/Images/Collections_About_2019/J%20collection/jcollection_desk_5.jpg',
                     id: 1,
-                    linkUrl: 'shop/chairs'
+                    linkUrl: 'chairs'
                   },
                   {
                     title: 'Lounge chairs',
                     imageUrl: 'https://www.fredericia.com/Files/Images/Collections_About_2019/The%20Spanish%20Collection/spanishcollectiondesktop8-min.jpg',
                     id: 2,
-                    linkUrl: 'shop/jackets'
+                    linkUrl: ''
                   },
                   {
                     title: 'Tables',
                     imageUrl: 'https://www.fredericia.com/Files/Images/Collections_About_2019/Piloti/piloticol_desk_2.jpg',
                     id: 3,
-                    linkUrl: 'shop/sneakers'
+                    linkUrl: ''
                   },
                   {
                     title: 'Dining',
                     imageUrl: 'https://www.fredericia.com/Files/Images/Collections_About_2019/The%20Spanish%20Collection/spanishcollectiondesktop6-min.jpg',
                     size: 'large',
                     id: 4,
-                    linkUrl: 'shop/womens'
+                    linkUrl: ''
                   },
                   {
                     title: 'Leisure',
                     imageUrl: 'https://www.fredericia.com/Files/Images/Collections_About_2019/calmo_7_ATC_1218x675px.jpg',
                     size: 'large',
                     id: 5,
-                    linkUrl: 'shop/mens'
+                    linkUrl: ''
                   }
             ]
         }
@@ -49,8 +49,7 @@ class Directory extends React.Component {
         return(
             <div className="directory-menu">
                 {                
-                this.state.sections.map(({title, id, imageUrl}) => <MenuItem key={id} title={title} imageUrl={imageUrl}/>)
-                }
+                this.state.sections.map(({id, ...otherSectionProps}) => (<MenuItem key={id} {...otherSectionProps} />))}
             </div>
         )
     }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
     return (
-        <div style={{
-            backgroundImage: `url(${imageUrl})`
-            }}
-            className="menu-item">
-            <div className="content">
-                <h1 className="title">{title}</h1>
-                <span className="subtitle">SHOP NOW</span>
+        <div className={`${size} menu-item`} 
+             onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            <div style={{
+                backgroundImage: `url(${imageUrl})`
+                }}
+                className="menu-item">
+                <div className="content">
+                    <h1 className="title">{title}</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
             </div>
         </div>
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem); 
